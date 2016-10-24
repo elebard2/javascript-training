4cb100091e60e76048cd0a9d817e3f502efe6e97
elebard2 <angry.n.gineer@gmail.com>
2016-10-24 14:57:02 +0200

    Excellent!

--- src/types/string.js
+++ src/types/string.js
@@ -18,8 +18,8 @@ console.log(isTrue, isFalse);
  */
 
 var sentence = "Hello World!"
-var tilltheend = sentence.substr(1);
-var inbetween = sentence.substring(1,6);
+var tilltheend = sentence.substr(1,6); /** contains the 6 characters from index #1*/
+var inbetween = sentence.substring(1,6); /** contains the characters between index #1 and #6*/
 
 console.log(tilltheend);
 console.log(inbetween);
